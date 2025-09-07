c6ce09f9bdb96c14b90310bc38f50fa75c220094
roger ramirez <rogier.riguetti@gmail.com>
2025-09-07 08:22:14 -0500

    commit_07092025
    actulizacion de fuente

--- routes/rutinaRoutes.js
+++ routes/rutinaRoutes.js
@@ -9,17 +9,16 @@ const reservasController = require('../controllers/reservasController');
 
 // Rutas de clientes
 
+router.post("/asignacionasesor", jwtAuth, cargasolController.AsignacionIntermediarios);
 router.post("/cargasol", jwtAuth,
     express.text({ type: ['application/xml'] }),
     cargasolController.ProcesaSolicitud);
-router.post("/asignacionasesor", jwtAuth, cargasolController.AsignacionIntermediarios);
 router.post("/calcular", jwtAuth, rutinaController.calcular);
 router.post('/generar-xml', jwtAuth, rutinaController.generaXMLsalida);
 router.post('/calcularpagos', jwtAuth, emisionController.ProcesoCalculoPagos);
 router.post('/calculareservas', jwtAuth, reservasController.ProcesoCalculoReservas);
 router.post('/consultar', jwtAuth, consultaController.consultar);
-
 //router.post("/calcularOfi", rutinaController.calcularofi);
-//router.post("/calcularOfi_optim", rutinaController.calcularofi_hilo);
+router.post("/calcularOfi_optim", rutinaController.calcularofi_hilo);
 
 module.exports = router;
